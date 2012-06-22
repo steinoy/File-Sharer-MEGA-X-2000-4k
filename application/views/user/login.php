2977eb6bb3d310baaf39c8a3c9f65a7cfde0daa5
Øystein Rød Tveito <hi@tastylittlenuggets.com>
2012-06-22 13:41:00 +0200

    Changed the login screen a little

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -7,7 +7,7 @@
 <section id="login" class="full-width login">
 	<div class="full-width login-actions">
 		
-		<?= HTML::anchor($login_url, 'Log in', array('class' => 'submit login-submit facebook', 'title' => 'Log in with Facebook', 'id' => 'fb-login')); ?>
+		<?= HTML::anchor($login_url, 'Log in with Facebook', array('class' => 'submit login-submit facebook', 'title' => 'Log in with Facebook', 'id' => 'fb-login')); ?>
 				 
 	</div>
 
