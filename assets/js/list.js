723fea15745f4f9814cca10d4669729127b0b8b0
Øystein Rød Tveito <hi@tastylittlenuggets.com>
2012-06-24 15:00:10 +0200

    Removed spinner

--- assets/js/list.js
+++ assets/js/list.js
@@ -33,7 +33,7 @@ EntryModel = Backbone.Model.extend({
         expires: 0,
         URI: '',
         fileNames: [],
-        deleteList: [],
+        deleteList: []
     },
 
     /**
@@ -176,23 +176,6 @@ EntryView = Backbone.View.extend({
         // 'blur input': 'onInputBlur'
     },
 
-    spinner: new Spinner({
-        lines: 17, // The number of lines to draw
-        length: 0, // The length of each line
-        width: 3, // The line thickness
-        radius: 33, // The radius of the inner circle
-        rotate: 0, // The rotation offset
-        color: '#000', // #rgb or #rrggbb
-        speed: 1, // Rounds per second
-        trail: 60, // Afterglow percentage
-        shadow: false, // Whether to render a shadow
-        hwaccel: false, // Whether to use hardware acceleration
-        className: 'spinner', // The CSS class to assign to the spinner
-        zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: 'auto', // Top position relative to parent in px
-        left: 'auto' // Left position relative to parent in px
-    }),
-
     /**
      * Expects an EntryModel to be sent along on initialization.
      * 
@@ -270,8 +253,6 @@ EntryView = Backbone.View.extend({
      * @return {EntryView}
      */
     save: function () {
-        this.spinner.spin(this.el);
-
         var that = this;
         var oldDeleteList = this.model.get('deleteList');
         var totalFilesSize = 0;
